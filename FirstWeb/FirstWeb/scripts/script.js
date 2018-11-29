01874964a874163942fba62fea86db12e8f9bbaa
XIN <1710911181@qq.com>
2018-11-29 10:22:46 +0800

    测试12

---
+++ FirstWeb/FirstWeb/scripts/script.js
@@ -0,0 +1,25 @@
+﻿var get = function (id) {
+    return document.getElementById(id);
+}
+var gets = function (name) {
+    return document.getElementsByTagName(name);
+}
+var c = function (id) {
+    return document.createElement(id);
+}
+onload = function () {
+    var image = c("img");
+    var dv = get("dv");
+    dv.setAttribute("width", "600px");
+    dv.setAttribute("style", "text-align:center");
+    dv.appendChild(image);
+    var ul = document.getElementsByTagName("ul")[0];
+    var a = ul.getElementsByTagName("a");
+    for (var i = 0; i < a.length; i++) {
+        a[i].onclick = function () {
+            image.src = this.href;
+            return false;
+        };
+    }
+
+};
